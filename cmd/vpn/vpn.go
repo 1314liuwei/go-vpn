beb89a58c76cde3fe105752cd904437519163704
lynx <1044945158@qq.com>
2022-09-02 20:26:09 +0800

    feature: vpn 测试成功

--- cmd/vpn/vpn.go
+++ cmd/vpn/vpn.go
@@ -10,6 +10,7 @@ type args struct {
 	lPort int
 	rAddr string
 	rPort int
+	dAddr string
 }
 
 func main() {
@@ -17,12 +18,14 @@ func main() {
 	flag.IntVar(&args.lPort, "lport", 18080, "local udp port")
 	flag.StringVar(&args.rAddr, "raddr", "127.0.0.1", "remote udp listen addr")
 	flag.IntVar(&args.rPort, "rport", 18081, "remote udp listen port")
+	flag.StringVar(&args.dAddr, "daddr", "127.0.0.1", "remote udp listen port")
 	flag.Parse()
 
 	e := engine.Engine{
 		LPort: args.lPort,
 		RAddr: args.rAddr,
 		RPort: args.rPort,
+		DAddr: args.dAddr,
 	}
 
 	err := e.Run()
--- engine/engine.go
+++ engine/engine.go
@@ -3,16 +3,18 @@ package engine
 import (
 	"go-vpn/conn"
 	"go-vpn/conn/udp"
+	"go-vpn/device"
 	"log"
 	"net/netip"
-	"time"
 )
 
 type Engine struct {
-	LPort int
-	RAddr string
-	RPort int
-	conn  *udp.Connection
+	LPort  int
+	RAddr  string
+	RPort  int
+	DAddr  string
+	conn   *udp.Connection
+	device device.Manager
 }
 
 func (e *Engine) Run() error {
@@ -22,40 +24,73 @@ func (e *Engine) Run() error {
 		return err
 	}
 
-	log.Println("start run...")
+	e.AddEndpoint(e.RAddr)
+	e.CreateDevice(e.DAddr)
+	go SendIPv4(e.conn, e.device)
+	go ReceiveIPv4(e.conn, e.device)
+	select {}
+}
 
-	addr, err := netip.ParseAddr(e.RAddr)
+func (e *Engine) CreateDevice(addr string) {
+	name := "tun0"
+	tun, err := device.CreateTUN(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//defer tun.Close()
+
+	wrap := device.NewManage(tun)
+	wrap.SetMTU(1280)
+	err = wrap.ChangeState(device.UP)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = wrap.SetAddrIPv4(addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	e.device = wrap
+}
+
+func (e *Engine) AddEndpoint(addr string) {
+	add, err := netip.ParseAddr(addr)
 	if err != nil {
 		panic(err)
 	}
-	e.conn.Notify(conn.Op{Action: "add", Value: netip.AddrPortFrom(addr, uint16(e.RPort))})
-
-	go SendIPv4(e.conn)
-	go ReceiveIPv4(e.conn)
-	select {}
-	return nil
+	e.conn.Notify(conn.Op{Action: "add", Value: netip.AddrPortFrom(add, uint16(e.RPort))})
 }
 
-func ReceiveIPv4(conn *udp.Connection) {
+func ReceiveIPv4(conn *udp.Connection, dev device.Manager) {
+	log.Println("start receive...")
 	for {
 		buff := make([]byte, 1024)
 		n, endpoint, err := conn.ReadFromUDPAddrPort(buff)
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Printf("%s:%d >> %s", endpoint.Addr(), endpoint.Port(), buff[:n])
-	}
-}
 
-func SendIPv4(conn *udp.Connection) {
-	log.Println("start send...")
-	for {
-		buff := []byte("hello world")
-		_, err := conn.Write(buff)
+		_, err = dev.Write(buff[:n])
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Println("send success!")
-		time.Sleep(time.Second)
+		log.Printf("%s:%d >> %v", endpoint.Addr(), endpoint.Port(), buff[:n])
+	}
+}
+
+func SendIPv4(conn *udp.Connection, dev device.Manager) {
+	log.Println("start send...")
+	for {
+		buff := make([]byte, 1024)
+		n, err := dev.Read(buff)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = conn.Write(buff[:n])
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("send >> %v", buff[:n])
 	}
 }
